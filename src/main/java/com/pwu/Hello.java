fca264a9f215bc9fa1f9e252a7f0f1b218954931
pwu <pwu@pipesh.co.jp>
2019-09-28 21:05:23 +0900

    test commit

--- src/main/java/com/pwu/Hello.java
+++ src/main/java/com/pwu/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         Person p = new Person();
         p.Hello();
+        byte i = -128;
     }
 }
--- src/main/kotlin/com/pwu/Hello.kt
+++ src/main/kotlin/com/pwu/Hello.kt
@@ -3,6 +3,8 @@ package com.pwu
 fun main(args: Array<String>) {
     val h = Human()
     h.Hello()
+    var name :String
+    name = "1"
 }
 
 class Human() {
